32991b30cfc19a4af9c053d4681ae58bc1c13edf
Rishabhltfb <rishabhsharma.rs0403@gmail.com>
2022-02-07 11:54:39 +0530

    chore: add utility/helper files and functions

---
+++ lib/constants/api_constants.dart
@@ -0,0 +1,6 @@
+// ignore_for_file: constant_identifier_names
+
+const AUTH_TOKEN = 'auth_token';
+const REFRESH_TOKEN = 'ref_token';
+const JWT_TOKEN_SECURE_STORAGE = "jwtToken";
+const REFRESH_TOKEN_SECURE_STORAGE = "refreshToken";
---
+++ lib/utils/app_utils.dart
@@ -0,0 +1,4 @@
+// ignore_for_file: constant_identifier_names
+
+const appName = "Glints Restaurant App";
+const appNameCapital = "GLINTS RESTAURANT APP";
