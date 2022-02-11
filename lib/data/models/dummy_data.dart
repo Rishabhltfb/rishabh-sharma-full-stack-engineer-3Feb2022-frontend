aaa4f54ebdfacda88157873edba60484af0011fa
Rishabhltfb <rishabhsharma.rs0403@gmail.com>
2022-02-12 02:07:30 +0530

    assets: add restaurant resources for app

--- lib/data/models/dummy_data.dart
+++ lib/data/models/dummy_data.dart
@@ -1,13 +1,24 @@
 import 'package:client/utils/assets.dart';
 
 class DummyData {
-  List<String> cussineList = [
-    RestaurantAssets.authBg,
-    RestaurantAssets.cuisine1,
-    RestaurantAssets.cuisine2,
-    RestaurantAssets.cuisine3,
-    RestaurantAssets.cuisine4,
-    RestaurantAssets.cuisine5,
-    RestaurantAssets.cuisine6,
+  List<Map<String, String>> cussineList = [
+    {"url": RestaurantAssets.cuisine1, 'name': 'american'},
+    {"url": RestaurantAssets.cuisine2, 'name': 'indian'},
+    {"url": RestaurantAssets.cuisine3, 'name': 'chinese'},
+    {"url": RestaurantAssets.cuisine4, 'name': 'italian'},
+    {"url": RestaurantAssets.cuisine5, 'name': 'mexican'},
+    {"url": RestaurantAssets.cuisine6, 'name': 'japanese'},
+    {"url": RestaurantAssets.cuisine1, 'name': 'american'},
+  ];
+  List<String> restaurantImageList = [
+    RestaurantAssets.restaurant1,
+    RestaurantAssets.restaurant2,
+    RestaurantAssets.restaurant3,
+    RestaurantAssets.restaurant4,
+    RestaurantAssets.restaurant5,
+    RestaurantAssets.restaurant6,
+    RestaurantAssets.restaurant7,
+    RestaurantAssets.restaurant8,
+    RestaurantAssets.restaurant9,
   ];
 }
--- lib/utils/assets.dart
+++ lib/utils/assets.dart
@@ -3,15 +3,28 @@ class RestaurantAssets {
 
   /// [Pictures]
   static const String authBg = 'assets/images/authBg.jpeg';
+  static const String authBg2 = 'assets/images/authBg2.jpeg';
+  static const String authBg3 = 'assets/images/authBg3.jpeg';
   static const String cuisine1 = 'assets/images/cuisine1.jpeg';
   static const String cuisine2 = 'assets/images/cuisine2.jpeg';
   static const String cuisine3 = 'assets/images/cuisine3.jpeg';
   static const String cuisine4 = 'assets/images/cuisine4.jpeg';
   static const String cuisine5 = 'assets/images/cuisine5.jpeg';
   static const String cuisine6 = 'assets/images/cuisine6.jpeg';
+  static const String restaurant1 = 'assets/images/restaurant1.jpg';
+  static const String restaurant2 = 'assets/images/restaurant2.jpg';
+  static const String restaurant3 = 'assets/images/restaurant3.jpg';
+  static const String restaurant4 = 'assets/images/restaurant4.jpg';
+  static const String restaurant5 = 'assets/images/restaurant5.jpg';
+  static const String restaurant6 = 'assets/images/restaurant6.jpg';
+  static const String restaurant7 = 'assets/images/restaurant7.jpg';
+  static const String restaurant8 = 'assets/images/restaurant8.jpg';
+  static const String restaurant9 = 'assets/images/restaurant9.jpg';
 
   /// [Icons]
-  static const String shimmerBg = 'assets/icons/shimmerBg.svg';
+  // static const String shimmerBg = 'assets/icons/shimmerBg.svg';
+  static const String splashLottieIcon =
+      'https://assets6.lottiefiles.com/private_files/lf30_i83aodu2.json';
 
   /// [Illustrations]
 
