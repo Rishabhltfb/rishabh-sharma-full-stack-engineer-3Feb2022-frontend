8705ceebbb7088fa56b93c8733959a35a820ae18
Rishabhltfb <rishabhsharma.rs0403@gmail.com>
2022-02-13 00:13:38 +0530

    style: app icon and name change

--- lib/ui/common_widgets/chips.dart
+++ lib/ui/common_widgets/chips.dart
@@ -1,3 +1,4 @@
+import 'package:client/utils/text_theme.dart';
 import 'package:flutter/material.dart';
 
 class Chips extends StatelessWidget {
@@ -8,7 +9,8 @@ class Chips extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.all(5),
-      width: 50,
+      width: 40,
+      height: 20,
       decoration: BoxDecoration(
         color: Colors.grey.withOpacity(0.5),
         borderRadius: const BorderRadius.all(
@@ -16,7 +18,7 @@ class Chips extends StatelessWidget {
         ),
       ),
       child: Center(
-        child: Text(text),
+        child: Text(text, style: kCaption2),
       ),
     );
   }
